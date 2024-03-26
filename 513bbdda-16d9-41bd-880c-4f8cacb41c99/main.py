3377d186b19305da44dee263a5ede0c8395564ff
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-25 20:38:47 -0400

    User Sam Brodsky saved changes to multiple files files at 03/26/2024, 00:38:46 UTC time.

--- 513bbdda-16d9-41bd-880c-4f8cacb41c99/main.py
+++ 513bbdda-16d9-41bd-880c-4f8cacb41c99/main.py
@@ -1,1 +1,20 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+
+
+class TradingStrategy(Strategy):
+
+    def __init__(self):
+        self.tickers = ["AAPL", ...]
+        self.data_list = [...]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
